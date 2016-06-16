c115d34eb88272d18b5db81e86948ac6b1878ba0
James Hamilton <jameshamilton9878@comcast.net>
2016-06-15 18:40:14 -0700

    New DataHelper method added to framework

---
+++ src/main/java/com/sqa/jh/DataHelper.java
@@ -0,0 +1,196 @@
+/**
+ *   File Name: DataHelper.java<br>
+ *
+ *   Hamilton, James<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 13, 2016
+ *   
+ */
+
+package com.sqa.jh;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+import com.sqa.jh.data.*;
+
+/**
+ * DataHelper //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Hamilton, James
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class DataHelper {
+
+	public static Object[][] getTextFileData(String fileLocation, String fileName, TextFormat textFormat) {
+		return getTextFileData(fileLocation, fileName, textFormat, false);
+	}
+
+	public static Object[][] getTextFileData(String fileLocation, String fileName, TextFormat textFormat,
+			Boolean hasLabels, Object... dataTypes) {
+		// Process data
+		Object[][] data;
+		// Collect data lines from text document supplied
+		ArrayList<String> lines = openFileAndCollectData(fileLocation, fileName);
+		switch (textFormat) {
+		case CSV:
+			data = parseCSVData(lines, hasLabels, dataTypes);
+			break;
+		case XML:
+			data = parseXMLData(lines, hasLabels);
+			break;
+		case TAB:
+			data = parseTabData(lines, hasLabels);
+			break;
+		case JSON:
+			data = parseJSONData(lines, hasLabels);
+			break;
+		default:
+			data = null;
+			break;
+		}
+		return data;
+	}
+
+	public static Object[][] getTextFileData(String fileLocation, String fileName, TextFormat textFormat,
+			Object... dataTypes) {
+		return getTextFileData(fileLocation, fileName, textFormat, false, dataTypes);
+	}
+
+	/**
+	 * @param group
+	 * @param class1
+	 * @return
+	 */
+	private static Object convertDataType(String parameter, Object dataType) {
+		if (dataType.equals(Integer.TYPE)) {
+			return Integer.parseInt(parameter);
+		} else if (dataType.equals(Boolean.TYPE)) {
+			return Boolean.getBoolean(parameter);
+		} else {
+			System.out.println("Data type is a String or not recognized, returning a String for (" + parameter + ")");
+			return parameter;
+		}
+	}
+
+	/**
+	 * @param fileLocation
+	 * @param fileName
+	 * @return
+	 */
+	private static ArrayList<String> openFileAndCollectData(String fileLocation, String fileName) {
+		// Create a full relative file path
+		String fullFilePath = fileLocation + fileName;
+		// Array to hold lines from file
+		ArrayList<String> dataLines = new ArrayList<String>();
+		try {
+
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(fullFilePath);
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			// Create String to hold current line
+			String line = bufferedReader.readLine();
+			// While there is a line to read or not null line
+			// (also setting line to current line)
+			while (line != null) {
+				// Pass current line to the gatherDataString Method
+				dataLines.add(line);
+				line = bufferedReader.readLine();
+			}
+			// Always close files.
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fullFilePath + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fullFilePath + "'");
+			// Alternative we could just do this:
+			// ex.printStackTrace();
+		}
+		return dataLines;
+	}
+
+	/**
+	 * @param lines
+	 * @param objects
+	 * @return
+	 */
+	private static Object[][] parseCSVData(ArrayList<String> lines, boolean hasLabels, Object[] dataTypes) {
+
+		ArrayList<Object> results = new ArrayList<Object>();
+		// Check for labels on first line
+		if (hasLabels) {
+			// Remove any labels present
+			lines.remove(0);
+		}
+
+		// String to be scanned to find the pattern.
+		String pattern = "(,*)([a-zA-Z0-9\\s]+)(,*)";
+
+		// Create a Pattern object
+		Pattern r = Pattern.compile(pattern);
+
+		for (int i = 0; i < lines.size(); i++) {
+			int curDataType = 0;
+			ArrayList<Object> curMatches = new ArrayList<Object>();
+			Matcher m = r.matcher(lines.get(i));
+			while (m.find()) {
+				if (dataTypes.length > 0) {
+					try {
+						curMatches.add(convertDataType(m.group(2), dataTypes[curDataType]));
+					} catch (Exception e) {
+						System.out.println("DataTypes provided do not match parsed data results.");
+					}
+				} else {
+					curMatches.add(m.group(2));
+				}
+				curDataType++;
+			}
+			Object[] resultsObj = new Object[curMatches.size()];
+			curMatches.toArray(resultsObj);
+			results.add(resultsObj);
+		}
+		System.out.println("Results:" + results);
+		Object[][] resultsObj = new Object[results.size()][];
+		results.toArray(resultsObj);
+		return resultsObj;
+	}
+
+	/**
+	 * @param lines
+	 * @param hasLabels
+	 * @return
+	 */
+	private static Object[][] parseJSONData(ArrayList<String> lines, Boolean hasLabels) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param lines
+	 * @param hasLabels
+	 * @return
+	 */
+	private static Object[][] parseTabData(ArrayList<String> lines, Boolean hasLabels) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param lines
+	 * @param hasLabels
+	 * @return
+	 */
+	private static Object[][] parseXMLData(ArrayList<String> lines, Boolean hasLabels) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
