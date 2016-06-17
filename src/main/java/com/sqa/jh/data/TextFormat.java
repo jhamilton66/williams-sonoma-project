323d9f6790fc272b9852610c583d619c4e8402c6
James Hamilton <jameshamilton9878@comcast.net>
2016-06-16 18:08:40 -0700

    Add clean up elements for newbranch

---
+++ src/main/java/com/sqa/jh/data/TextFormat.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: TextFormat.java<br>
+ *
+ *   Hamilton, James<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 13, 2016
+ *   
+ */
+
+package com.sqa.jh.data;
+
+/**
+ * TextFormat //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Hamilton, James
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public enum TextFormat {
+	CSV, JSON, TAB, XML
+}
