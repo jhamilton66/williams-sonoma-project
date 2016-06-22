db4d5b3252708842b15f352b737af58349851d51
James Hamilton <jameshamilton9878@comcast.net>
2016-06-21 21:39:36 -0700

    Updating POM and DataHelper files

--- src/main/java/com/sqa/jh/DataHelper.java
+++ src/main/java/com/sqa/jh/DataHelper.java
@@ -11,6 +11,7 @@
 package com.sqa.jh;
 
 import java.io.*;
+import java.sql.*;
 import java.util.*;
 import java.util.regex.*;
 
@@ -32,6 +33,59 @@ import com.sqa.jh.data.*;
  *
  */
 public class DataHelper {
+
+	public static Object[][] evalDatabaseTable(String driverClassString, String databaseStringUrl, String username,
+			String password, String tableName) throws ClassNotFoundException, SQLException {
+
+		return evalDatabaseTable(driverClassString, databaseStringUrl, username, password, tableName, 0, 0);
+	}
+
+	public static Object[][] evalDatabaseTable(String driverClassString, String databaseStringUrl, String username,
+			String password, String tableName, int rowOffset, int colOffset)
+			throws ClassNotFoundException, SQLException {
+
+		Object[][] myData;
+		ArrayList<Object> myArrayData = new ArrayList<Object>();
+
+		Class.forName(driverClassString);
+
+		Connection dbconn = DriverManager.getConnection(databaseStringUrl, username, password);
+
+		Statement stmt = dbconn.createStatement();
+
+		ResultSet rs = stmt.executeQuery("select * from " + tableName);
+
+		int numOfColumns = rs.getMetaData().getColumnCount();
+		int curRow = 1;
+
+		while (rs.next()) {
+			if (curRow > rowOffset) {
+				Object[] rowData = new Object[numOfColumns - colOffset];
+
+				for (int i = 0, j = colOffset; i < rowData.length; i++) {
+					rowData[i] = rs.getString(i + colOffset + 1);
+				}
+
+				myArrayData.add(rowData);
+
+			}
+			curRow++;
+
+		}
+
+		myData = new Object[myArrayData.size()][];
+
+		for (int i = 0; i < myData.length; i++) {
+			myData[i] = (Object[]) myArrayData.get(i);
+		}
+
+		// Step 5
+		rs.close();
+		stmt.close();
+		dbconn.close();
+
+		return myData;
+	}
 
 	public static Object[][] getExcelFileData(String fileLocation, String fileName, Boolean hasLabels) {
 		Object[][] data = { { "Square", 36, 6, 0 }, { "Rectangle", 12, 4, 3 } };
