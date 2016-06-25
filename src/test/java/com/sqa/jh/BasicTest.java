cc5b32d2fc0c1369f36c870c80fd11ecfca799d0
James Hamilton <jameshamilton9878@comcast.net>
2016-06-25 13:03:14 -0700

    Updated Basic Test with Alert Handling

--- src/test/java/com/sqa/jh/BasicTest.java
+++ src/test/java/com/sqa/jh/BasicTest.java
@@ -45,6 +45,8 @@ public class BasicTest {
 		driver.quit();
 	}
 
+	private boolean acceptNextAlert = true;
+
 	/**
 	 *
 	 */
@@ -59,6 +61,42 @@ public class BasicTest {
 		getDriver().manage().deleteAllCookies();
 		getDriver().get(getBaseURL());
 
+	}
+
+	@SuppressWarnings("unused")
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private boolean isElementPresent(By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
 	}
 
 }
